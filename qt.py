41db2e5494845e0b0409149b78e0bf40fa946cd8
Organick <mcagriaksoy@ya.ru>
2018-08-14 23:29:41 +0300

    Tuesday update
    NOW! System runs correctly, still needs performance improvements

--- qt.py
+++ qt.py
@@ -3,13 +3,20 @@ __author__ = 'Mehmet Cagri Aksoy - github.com/mcagriaksoy'
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtCore import pyqtSlot, QSize, QRect 
-from PyQt5.QtWidgets import QApplication, QComboBox, QDialog, QMainWindow, QWidget, QLabel, QTextEdit, QListWidget
+from PyQt5.QtWidgets import QApplication, QComboBox, QDialog, QMainWindow, QWidget, QLabel, QTextEdit, QListWidget,QListView
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon, QPixmap
 import sys, time, threading
 from random import randint
 import serial
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+#ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial(
+    port='/dev/ttyUSB0',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
 class qt(QMainWindow):	
 
 	def __init__(self):
@@ -24,6 +31,8 @@ class qt(QMainWindow):
 		
 
 	def on_pushButton_clicked(self):
+		#if self.x==0 :
+			#ser.open()
 		self.completed = 0
 		while self.completed < 100 :
 			self.completed += 0.001
@@ -32,14 +41,20 @@ class qt(QMainWindow):
 		self.label_5.setText(self.combo.currentText())
 		#self.label_5.setText('OK!')
 		self.label_5.setStyleSheet('color: green')
-		print(ser.readline())
-		
-	
+		x = 1
+		line=[]
+		while x<100 :
+			x+=x
+			line=ser.readline().decode('ascii')
+			print(line)
+			self.textEdit_3.append(line)
+		#self.listWidget.insertItems(ser.readline())	
 	def on_pushButton_3_clicked(self):
 		mytext = self.textEdit_2.toPlainText()
 		ser.write(mytext.encode())
 	def on_pushButton_2_clicked(self):
-		self.x=0
+		self.x=0		
+		#ser.close()
 		self.textEdit.setText('Durduruldu! Yeniden Baglanmak icin BAGLAN-a basin...')
 
 				
