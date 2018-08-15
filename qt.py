25d15da0edebeb78c0ae89421d07b24fe31b3bcd
Organick <mcagriaksoy@ya.ru>
2018-08-16 02:41:04 +0300

    First beta release
    Huge improvements.
    New feature, save .txt file

--- qt.py
+++ qt.py
@@ -1,93 +1,84 @@
 __author__ = 'Mehmet Cagri Aksoy - github.com/mcagriaksoy'
 
 import sys, serial, threading, time
-from PyQt5.QtCore import QSize, QRect, QObject, pyqtSignal, QThread
+from PyQt5.QtCore import QSize, QRect, QObject, pyqtSignal, QThread, pyqtSignal, pyqtSlot
+import time
 from PyQt5.QtWidgets import QApplication, QComboBox, QDialog, QMainWindow, QWidget, QLabel, QTextEdit, QListWidget, \
     QListView
 from PyQt5.uic import loadUi
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-
-end = False
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
 
 
-class Foo(QThread):
+# class Foo(QThread):
 
-    def connect(self):
-        connect(pushButton_2, SIGNAL("clicked()"), on_pushButton_2_clicked)
-
+#    def connect(self):
+#        connect(pushButton_2, SIGNAL("clicked()"), on_pushButton_2_clicked)
 
 # MULTI-THREADING
 class Worker(QObject):
-    log = pyqtSignal(str)
-    finished = pyqtSignal(str)
+    finished = pyqtSignal()
+    intReady = pyqtSignal(str)
 
+    @pyqtSlot()
     def __init__(self):
         super(Worker, self).__init__()
         self.working = True
+
     def work(self):
-
         while self.working:
-
             line = ser.readline().decode('utf-8')
             print(line)
-            self.log.emit(self.line)
-            #if line != '':
-            #    self.textEdit_3.append(line)
-            time.sleep(1)
-        #self.finished.emit()
+            time.sleep(0.1)
+            self.intReady.emit(line)
+            # if line != '':
+            # self.textEdit_3.append(line)
+
+        self.finished.emit()
 
 
 class qt(QMainWindow):
 
     def __init__(self):
 
-        self.thread = QThread()
-        self.worker = Worker()
-        self.worker.moveToThread(self.thread)
-
         QMainWindow.__init__(self)
         loadUi('qt.ui', self)
         self.comboBox_0.activated.connect(self.load_value0)
         self.comboBox_1.activated.connect(self.load_value1)
-        self.x = 0
-
-
         self.thread = None
         self.worker = None
         self.pushButton.clicked.connect(self.start_loop)
 
-
     def loop_finished(self):
         print('Looped Finished')
 
-    def start_loop(self, textEdit_4=None):
-        global line
-        self.thread = QThread()
-        self.thread.start()
-        self.thread.log.connect(self.aa)
-        self.but1.setEnabled(False)
+    def start_loop(self):
 
-        self.thread = QThread()  # a new thread to run our background tasks in
         self.worker = Worker()  # a new worker to perform those tasks
+        self.thread = QThread()  # a new thread to run our background tasks in
         self.worker.moveToThread(
             self.thread)  # move the worker into the thread, do this first before connecting the signals
 
-        self.thread.started.connect(self.worker.work)  # begin our worker object's loop when the thread starts running
+        self.thread.started.connect(self.worker.work)
+        # begin our worker object's loop when the thread starts running
+
+        self.worker.intReady.connect(self.onIntReady)
+
         self.pushButton_2.clicked.connect(self.stop_loop)  # stop the loop on the stop button click
+
         self.worker.finished.connect(self.loop_finished)  # do something in the gui when the worker loop ends
         self.worker.finished.connect(self.thread.quit)  # tell the thread it's time to stop running
         self.worker.finished.connect(self.worker.deleteLater)  # have worker mark itself for deletion
         self.thread.finished.connect(self.thread.deleteLater)  # have thread mark itself for deletion
         # make sure those last two are connected to themselves or you will get random crashes
-
         self.thread.start()
-
-    def aa(self):
-        self.textEdit_3.append(line)
 
     def stop_loop(self):
         self.worker.working = False
+
+    def onIntReady(self, i):
+        self.textEdit_3.append("{}".format(i))
+        print(i)
 
     def load_value0(self, index0):
         # portadi
@@ -98,53 +89,45 @@ class qt(QMainWindow):
     def load_value1(self, index1):
         # baudrate
         b2 = self.comboBox_1.itemText(index1)
-
         print(self.comboBox_1.itemText(index1))
         self.x = 1
 
     def on_pushButton_4_clicked(self):
-        if self.x is not 0:
-            def create_serial_obj(a2, b2):
-
-                return serial.Serial(a2, b2)
-
-            print('basarili')
+        if self.x != 0:
+            ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+            # print('basarili')
         else:
-            print('hata')
+            # print('hata')
             self.textEdit.setText('Lütfen Port ve Hızı girin!')
+
+    # TXT YAZDIRMA ------ KAYDETME
+    def on_pushButton_5_clicked(self):
+        with open('Sonuc.txt', 'w') as f:
+            my_text = self.textEdit_3.toPlainText()
+            f.write(my_text)
 
     def on_pushButton_2_clicked(self):
         self.textEdit.setText('Durduruldu! Yeniden Baglanmak icin BAGLAN-a basin...')
 
     def on_pushButton_clicked(self):
-        # if self.x==0 :
-        # ser.open()
-        self.completed = 0
 
+        self.completed = 0
         while self.completed < 100:
             self.completed += 0.001
             self.progressBar.setValue(self.completed)
         self.textEdit.setText('Veri Aliniyor...')
+
         # self.label_5.setText('OK!')
+        self.label_5.setText(index1)
         self.label_5.setStyleSheet('color: green')
         x = 1
         self.textEdit_3.setText(":")
 
-
-
-
-    # exception code here
-    # self.listWidget.insertItems(ser.readline())
-
     def on_pushButton_3_clicked(self):
 
         mytext = self.textEdit_2.toPlainText()
+        print(mytext.encode())
         ser.write(mytext.encode())
-
-    # def on_pushButton_2_clicked(self):
-
-    # ser.close()
-    #   self.textEdit.setText('Durduruldu! Yeniden Baglanmak icin BAGLAN-a basin...')
 
 
 def run():
