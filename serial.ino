ade7f975722bd4143e5e92f6d7635f3ab62caa14
Organick <mcagriaksoy@ya.ru>
2018-08-17 16:42:02 +0300

    serial-test code for arduino side
    serial-test code for arduino side

---
+++ serial.ino
@@ -0,0 +1,17 @@
+void setup() {
+Serial.begin(9600); // set the baud rate
+Serial.println("Ready"); // print "Ready" once
+}
+void loop() {
+char inByte = ' ';
+
+/*for(int x=0; x<10;x++){
+  Serial.println(x);
+  delay(3000);
+  } */
+if(Serial.available()){ 
+String(inByte) = Serial.readString(); 
+Serial.println(inByte); // send the data back in a new line so that it is not all one long line
+}
+delay(1); // delay for 1/10 of a second
+}
