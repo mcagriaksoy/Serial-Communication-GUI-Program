6dbd458fb05a97a81fb5b580fec8df319a008e90
Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>
2023-10-18 14:05:44 +0200

    Complete bug fixes on program, old theme restored
    Signed-off-by: Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>

--- src/ui_main.py
+++ src/ui_main.py
@@ -1,326 +1,165 @@
 __author__ = 'Mehmet Cagri Aksoy - github.com/mcagriaksoy'
+__annotations__ = 'Serial Communication GUI Program'
 
-import os
+# IMPORTS
 import sys
-import time
 
-import serial
-import serial.tools.list_ports
-from PyQt6.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
-from PyQt6.QtWidgets import (QApplication, QFileDialog,  # , QProgressBar
-                             QMainWindow, QMessageBox)
-from PyQt6.uic import loadUi
+try:
+    import serial
+    import serial.tools.list_ports
+    from serial import SerialException
+    from PyQt6.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
+    from PyQt6.QtWidgets import (QApplication, QFileDialog,
+                                QMainWindow, QMessageBox)
+    from PyQt6.uic import loadUi
+except ImportError as e:
+    print("Import Error! Please install the required libraries: " + str(e))
+    sys.exit(1)
+# IMPORTS END
+# GLOBAL VARIABLES
+SERIAL_INFO = serial.Serial()
 
+#Port Detection START
+try:
+    ports = [
+        p.device
+        for p in serial.tools.list_ports.comports()
+        if 'USB' in p.description
+    ]
+
+    if not ports:
+        print("There is no device exist on serial port!")
+        ports = "-"
+    elif len(ports) > 1:
+        print("Available serial ports are: " + str(ports))
+        SERIAL_INFO = serial.Serial(ports[0], 9600)
+except SerialException as e:
+    print("Serial Exception! Please check the serial port: " + str(e))
+    sys.exit(1)
+#Port Detection END
 
 # MULTI-THREADING
 class Worker(QObject):
+    """ Worker Thread """
     finished = pyqtSignal()
-    intReady = pyqtSignal(str)
+    serial_data = pyqtSignal(str)
 
-    @pyqtSlot() # use a decorator to indicate that this method is a slot
-    def __init__(self): # define the constructor method of the class
-        super(Worker, self).__init__() # call the constructor of the parent class
-        self.working = True # set an attribute named working to True
-
-    def work(self): # define a method named work that will perform the main task of the worker
-        while self.working: # while the working attribute is True
-            if ser.isOpen(): # if the serial port is open
-                line = ser.readline().decode('utf-8') # read a line from the port and decode it as utf-8
-            else:
-                line = '' # otherwise, set the line to an empty string
-
-            if line != '': # if the line is not empty
-                time.sleep(0.1) # wait for 0.1 seconds
-                self.intReady.emit(line) # emit the line as a signal
-
-        self.finished.emit() # when the loop ends, emit the finished signal
-
-class main_window(QMainWindow):
-
+    @pyqtSlot()
     def __init__(self):
+        super(Worker, self).__init__()
+        self.working = True
 
+    def work(self):
+        """ Read data from serial port """
+        while self.working:
+            try:
+                char = SERIAL_INFO.read()
+                h = char.hex()
+                self.serial_data.emit(h)
+            except SerialException:
+                # Emit last error message before die!
+                self.serial_data.emit("ERROR_SERIAL_EXCEPTION")
+                self.working = False
+            self.finished.emit()
+
+class MainWindow(QMainWindow):
+    """ Main Window """
+    def __init__(self):
+        """ Initialize Main Window """
         QMainWindow.__init__(self)
-        loadUi('../ui/main_window.ui', self)
+
+        loadUi('..\\ui\\main_window.ui', self)
 
         self.thread = None
         self.worker = None
-        self.pushButton.clicked.connect(self.start_loop)
-        self.pushBtnClicked = False
-        self.CopyFlag = 0
+        self.start_button.clicked.connect(self.start_loop)
+        self.label_11.setText(ports[0])
+
+    def print_message_on_screen(self, text):
+        """ Print the message on the screen """
+        msg = QMessageBox()
+        msg.setWindowTitle("Warning!")
+        msg.setIcon(QMessageBox.Icon.Warning)
+        msg.setText(text)
+        msg.exec()
 
     def start_loop(self):
-        #Verify the correct COM Port
+        """ Start the loop """
         try:
-            mytext = "\n"  # Send first enter
-            global ser
-            ser = serial.Serial(portname, 115200, timeout=1)  # (ports[0], 115200)    #('COM1', 115200, timeout=1)
-            ser.write(mytext.encode())
-        except:
-            msgBox = QMessageBox()
-            msgBox.setWindowTitle("COM Port Error!")
-            #msgBox.setIcon(QMessageBox.Warning)
-            msgBox.setText("Selected COM port does not exist. Please verify the COM port Number.")
-            msgBox.exec()
-            self.label_5.setText("Not Connected")
-            self.label_5.setStyleSheet('color: red')
-            return
-
-        self.worker = Worker()   # a new worker to perform those tasks
-        self.thread = QThread()  # a new thread to run our background tasks in
-        self.worker.moveToThread(self.thread)  # move the worker into the thread, do this first before connecting the signals
-
-        self.thread.started.connect(self.worker.work) # begin our worker object's loop when the thread starts running
-
-        self.worker.intReady.connect(self.onIntReady)
-
-        self.pushButton_2.clicked.connect(self.stop_loop)      # stop the loop on the stop button click
-
-        self.worker.finished.connect(self.thread.quit)         # tell the thread it's time to stop running
-        self.worker.finished.connect(self.worker.deleteLater)  # have worker mark itself for deletion
-        self.thread.finished.connect(self.thread.deleteLater)  # have thread mark itself for deletion
-        self.thread.start()
+            self.worker = Worker()   # a new worker to perform those tasks
+            self.thread = QThread()  # a new thread to run our background tasks in
+            # move the worker into the thread, do this first before connecting the signals
+            self.worker.moveToThread(self.thread)
+            # begin our worker object's loop when the thread starts running
+            self.thread.started.connect(self.worker.work)
+            self.worker.serial_data.connect(self.read_data_from_thread)
+            # stop the loop on the stop button click
+            self.end_button.clicked.connect(self.stop_loop)
+            # tell the thread it's time to stop running
+            self.worker.finished.connect(self.thread.quit)
+            # have worker mark itself for deletion
+            self.worker.finished.connect(self.worker.deleteLater)
+            # have thread mark itself for deletion
+            self.thread.finished.connect(self.thread.deleteLater)
+            self.thread.start()
+        except RuntimeError:
+            self.print_message_on_screen("Exception in Worker Thread!")
 
     def stop_loop(self):
+        """ Stop the loop """
         self.worker.working = False
-        self.label_5.setText("Not Connected")
-        self.label_5.setStyleSheet('color: red')
-        ser.close()
+        self.textEdit.setText('Stopped!')
 
-    def onIntReady(self, i):
-        if i != '':
-            self.textEdit_3.insertPlainText("{}".format(i))
-            if self.ck_AuSC.isChecked():
-                #Auto Copy files
-                if i.find('io copy j:') != -1:
-                    #First autocopy condition
-                    self.CopyFlag = 1
+    def read_data_from_thread(self, serial_data):
+        """ Write the result to the text edit box"""
+        # self.textEdit_3.append("{}".format(i))
+        if "ERROR_SERIAL_EXCEPTION" in serial_data:
+            self.print_message_on_screen("Serial Exception! Please check the serial port")
+            self.label_5.setText("NOT CONNECTED!")
+            self.label_5.setStyleSheet('color: red')
+        else:
+            self.comboBox.setEnabled(False)
+            self.comboBox_2.setEnabled(False)
+            self.comboBox_3.setEnabled(False)
+            self.start_button.setEnabled(False)
 
-                if i.find('nvdbg>') != -1 and self.CopyFlag == 1:
-                    count = self.sb_Num.value()
-                    if count < 10:
-                        numb = '0' + str(count)
-                    else:
-                        numb = str(count)
-
-                    mytext = "io copy j:" + self.txtIQfile.toPlainText() + numb + ".iq " + self.cb_Drive.currentText() + "\n"
-                    ser.write(mytext.encode())
-
-                    #Check for final file copy
-                    if count == self.sb_NumFin.value():
-                        self.ck_AuSC.setCheckState(False)
-                        self.sb_NumFin.setEnabled(False)
-                        self.CopyFlag = 0
-                        mytext = "\n"       #Clear buffer
-                        ser.write(mytext.encode())
-                    else:
-                        self.sb_Num.setValue(count + 1)
-
-
-    # TXT Save
-    def on_pushButton_5_clicked(self):
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-
-        fileName = QFileDialog.getSaveFileName(self, 'Select location to Log', "", '*.txt')
-        if not fileName:
-            return
-
-        with open(fileName[0], 'w') as f:
-            my_text = self.textEdit_3.toPlainText()
-            f.write(my_text)
-
-        self.textEdit_3.append("Log Saved in :" + fileName[0] + "\r\n")
-        self.pushBtnClicked = True
-
-    def on_pushButton_2_clicked(self):
-        self.textEdit.setText('Stopped! Please click CONNECT...')
-
-    def on_pb_Clr_clicked(self):
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-
-        #Clear serial screen
-        self.textEdit_3.setText('')
-        mytext = "\n"  # Clear buffer
-        ser.write(mytext.encode())
-
-        self.pushBtnClicked = True
-
-    def on_pushButton_clicked(self):
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-
-        # Port Detection START
-        ports = [
-            p.device
-            for p in serial.tools.list_ports.comports()
-            if 'USB' in p.description
-        ]
-
-        if not ports:
-            ports = ['NONE']
-
-        self.label_11.setText(ports[0])
-        # Port Detection END
-        global portname
-        portname = ports[0]
-        if ports[0] != 'NONE':
-            #Start the progress bar
-            self.completed = 0
-            while self.completed < 100:
-                self.completed += 1
-                self.progressBar.setValue(self.completed)
             self.textEdit.setText('Data Gathering...')
             self.label_5.setText("CONNECTED!")
             self.label_5.setStyleSheet('color: green')
-            x = 1
+            self.textEdit_3.insertPlainText("{}".format(serial_data))
 
-        self.pushBtnClicked = True
-
-
-    def on_pushButton_3_clicked(self):
-        # Send data from serial port:
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-        mytext = self.textEdit_2.toPlainText() + "\n"
-        ser.write(mytext.encode())
-        self.pushBtnClicked = True
-
-    def on_pb_Free_clicked(self):
-        # Send Freeze command from serial port:
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-        mytext = "cine freeze\n"
-        ser.write(mytext.encode())
-        self.pushBtnClicked = True
-
-    def on_ck_AuSC_clicked(self):
-        # Auto Store/Copy active
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-
-        if self.ck_AuSC.isChecked():
-            self.sb_NumFin.setEnabled(True)
+    # Save the settings
+    def on_save_button_clicked(self):
+        """ Save the settings """
+        if self.x != 0:
+            self.textEdit.setText('Settings Saved!')
         else:
-            self.sb_NumFin.setEnabled(False)
+            self.textEdit.setText('Please enter port and speed!')
 
-        self.pushBtnClicked = True
+    def on_save_txt_button_clicked(self):
+        """ Save the values to the TXT file"""
+        with open('Output.txt', 'w', encoding='utf-8') as f:
+            my_text = self.textEdit_3.toPlainText()
+            f.write(my_text)
+            f.close()
 
-    def on_pb_Unfre_clicked(self):
-        # Send Unfreeze command from serial port:
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-        mytext = "cine unfreeze\n"
-        ser.write(mytext.encode())
-        self.pushBtnClicked = True
+    def on_end_button_clicked(self):
+        """ Stop the process """
+        self.textEdit.setText('Stopped!')
+        self.comboBox.setEnabled(True)
+        self.comboBox_2.setEnabled(True)
+        self.comboBox_3.setEnabled(True)
+        self.start_button.setEnabled(True)
 
-    def on_pb_List_clicked(self):
-        # Send List dir command from serial port:
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-        mytext = "io dir " + self.cb_Drive.currentText() + "\n"
-        ser.write(mytext.encode())
-        self.pushBtnClicked = True
-
-    def on_pb_Brow_clicked(self):
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-        # select folder with files to rename
-        folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        if not folder:
-            return
-
-        self.txt_Dir.setText(folder)
-        self.pushBtnClicked = True
-
-    def on_pb_Rena_clicked(self):
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-
-        if self.txtIQfile.toPlainText() == "" or self.txt_Dir.toPlainText() == "":
-            msgBox = QMessageBox()
-            msgBox.setWindowTitle("No IQ File Name!")
-            msgBox.setIcon(QMessageBox.Information)
-            msgBox.setText("There is not an IQ File Name or Directory. Please specify the File Name and Directory first.")
-            msgBox.exec()
-            self.pushBtnClicked = True
-            return
-
-        # go across files and rename those
-        folder = self.txt_Dir.toPlainText()
-        for count, filename in enumerate(os.listdir(folder)):
-            if count < 10:
-                numb = '0' + str(count)
-            else:
-                numb = str(count)
-
-            dst = self.txtIQfile.toPlainText() + numb + ".mov"
-            src = folder + '/' + filename
-            dst = folder + '/' + dst
-
-            os.rename(src, dst)
-
-        self.pushBtnClicked = True
-
-    def on_pb_StCo_clicked(self):
-        if self.pushBtnClicked:
-            self.pushBtnClicked = False
-            return
-
-        if self.txtIQfile.toPlainText() == "":
-            msgBox = QMessageBox()
-            msgBox.setWindowTitle("No IQ File Name!")
-            msgBox.setIcon(QMessageBox.Information)
-            msgBox.setText("There is not an IQ File Name. Please specify the File Name first.")
-            msgBox.exec()
-            self.pushBtnClicked = True
-            return
-
-        # Check if auto-increment index
-        if self.ck_Auto.isChecked():
-            count = self.sb_Num.value()
-            if count < 10:
-                numb = '0' + str(count)
-            else:
-                numb = str(count)
-
-            #check for store command
-            if self.ch_Store.isChecked():
-                mytext = "cine store j:" + self.txtIQfile.toPlainText() + numb + ".iq 2363 2442 2 1\n"
-                ser.write(mytext.encode())
-
-            # check for copy command
-            if self.ch_Copy.isChecked():
-                mytext = "io copy j:" + self.txtIQfile.toPlainText() + numb + ".iq " + self.cb_Drive.currentText() + "\n"
-                ser.write(mytext.encode())
-
-            self.sb_Num.setValue(count+1)
-
-        else:
-            # check for store command
-            if self.ch_Store.isChecked():
-                mytext = "cine store j:" + self.txtIQfile.toPlainText() + ".iq 2363 2442 2 1\n"
-                ser.write(mytext.encode())
-
-            # check for copy command
-            if self.ch_Copy.isChecked():
-                mytext = "io copy j:" + self.txtIQfile.toPlainText() + ".iq " + self.cb_Drive.currentText() + "\n"
-                ser.write(mytext.encode())
-
-        self.pushBtnClicked = True
+    def on_send_data_button_clicked(self):
+        """ Send data to serial port """
+        mytext = self.textEdit_2.toPlainText()
+        print(mytext.encode())
+        SERIAL_INFO.write(mytext.encode())
 
 def start_ui_design():
+    """ Start the UI Design """
     app = QApplication(sys.argv)
-    widget = main_window()
+    widget = MainWindow()
     widget.show()
     sys.exit(app.exec())
